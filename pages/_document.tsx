eb922bf17be824eba581c4e0df67303c9717f85e
Adam Young <adamyoungg97@gmail.com>
2022-07-21 20:26:00 +0100

    Added gtag

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from 'next';
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+
+const Document: NextPage = () => {
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-N6DG2D1VYH" strategy="afterInteractive" />
+                <Script
+                    id="gtag"
+                    dangerouslySetInnerHTML={{
+                        __html: " window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-N6DG2D1VYH');",
+                    }}
+                    strategy="afterInteractive"
+                />
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default Document;
