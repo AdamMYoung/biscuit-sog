48937c746093c9a15cbb03fc312d3b7b75a551a1
Adam Young <adamyoungg97@gmail.com>
2022-08-18 20:08:02 +0100

    Fix to tagging

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,13 +7,13 @@ const Document: NextPage = () => {
         <Html>
             <Head />
             <body>
-                <Script src="https://www.googletagmanager.com/gtag/js?id=G-N6DG2D1VYH" strategy="afterInteractive" />
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-N6DG2D1VYH" strategy="beforeInteractive" />
                 <Script
                     id="gtag"
                     dangerouslySetInnerHTML={{
                         __html: " window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-N6DG2D1VYH');",
                     }}
-                    strategy="afterInteractive"
+                    strategy="beforeInteractive"
                 />
                 <Script
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732665435466396"
