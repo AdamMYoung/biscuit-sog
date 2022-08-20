bf2cc224d52e67c1a33b9c79b0ab6df3a4c8da7f
Adam Young <adamyoungg97@gmail.com>
2022-08-21 00:12:54 +0100

    Fix to adsense tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,13 @@ import Script from 'next/script';
 const Document: NextPage = () => {
     return (
         <Html>
-            <Head />
+            <Head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732665435466396"
+                    crossOrigin="anonymous"
+                ></script>
+            </Head>
             <body>
                 <Script src="https://www.googletagmanager.com/gtag/js?id=G-N6DG2D1VYH" strategy="beforeInteractive" />
                 <Script
@@ -13,11 +19,6 @@ const Document: NextPage = () => {
                     dangerouslySetInnerHTML={{
                         __html: " window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-N6DG2D1VYH');",
                     }}
-                    strategy="beforeInteractive"
-                />
-                <Script
-                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732665435466396"
-                    crossOrigin="anonymous"
                     strategy="beforeInteractive"
                 />
 
